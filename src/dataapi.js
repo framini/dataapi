e570a723977274a8c11cf7522154b8755f740900
Francisco Ramini <framini@gmail.com>
2016-05-31 13:31:01 -0300

    added basic skeleton to pass the tests

--- src/dataapi.js
+++ src/dataapi.js
@@ -1,1 +1,18 @@
+const dataapi = (config) => {
+  return {
+    defineSyncComponents,
+    start,
+    stop
+  }
+}
 
+function defineSyncComponents() {
+}
+
+function start() {
+}
+
+function stop() {
+}
+
+module.exports = dataapi;

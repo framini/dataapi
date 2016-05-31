688441908c32d7537463423709cdf7fca8324cd1
Francisco Ramini <framini@gmail.com>
2016-05-31 12:28:16 -0300

    changed filenames for the dist files

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ switch (process.env.npm_lifecycle_event) {
         devtool: 'source-map',
         output: {
           path: PATHS.dist,
-          filename: '[name].[chunkhash].js',
+          filename: '[name].js',
           // This is used for require.ensure. The setup
           // will work without but this is useful to set.
           chunkFilename: '[chunkhash].js'

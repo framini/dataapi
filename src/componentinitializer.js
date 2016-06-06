1dcacc84eccf54b121610d375c155e8d779eef5e
Francisco Ramini <framini@gmail.com>
2016-06-06 12:16:45 -0300

    add passing implementation for the component initializer

--- src/componentinitializer.js
+++ src/componentinitializer.js
@@ -1,8 +1,32 @@
-function getInitializedComponents() {
+function getInitializedComponents(config) {
+  return config.cache;
 }
 
-export default function componentInitializer() {
+function __initComponents(config) {
+  // all of them should be Maps
+  const { factories, components, options, cache } = config;
+
+  for (const [el, obj] of components) {
+    const compName = obj.name;
+    const factory = factories.get(compName);
+    if (factory !== undefined) {
+      const instance = factory(options);
+      instance.init(obj);
+      cache.set(el, instance);
+    }
+  }
+
+  return cache;
+}
+
+export default function componentInitializer(cfg) {
+  const config = Object.assign({}, {
+    cache: new Map(),
+  }, cfg);
+
+  __initComponents(config);
+
   return {
-    getInitializedComponents,
+    getInitializedComponents: getInitializedComponents.bind(null, config),
   };
 }

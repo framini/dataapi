58ddb48c4a09129b0181bddc0f224f9129dee2cc
Francisco Ramini <framini@gmail.com>
2016-06-06 19:39:06 -0300

    added first implementation for the public API

--- src/dataapi.js
+++ src/dataapi.js
@@ -1,27 +1,86 @@
 import { defaults } from 'lodash';
-import component from './componentparser';
+import componentParser from './componentparser';
+import componentHandler from './componenthandler';
 
-function start() {
+function start(cfg) {
+  const call = new Promise((resolve, rejected) => {
+    const config = defaults(cfg, {
+      resolve,
+      rejected,
+    });
+    let components;
+
+    // if the componets were passed as an argument, we'll use that instead
+    if (config.components === undefined) {
+      config.components = componentParser(config).parseComponents();
+    }
+
+    try {
+      components = componentHandler(config);
+      cache.set('componentHandler', components);
+      const initializedComponents = components.getInitializedComponents();
+      cache.set('initializedComponents', initializedComponents);
+      resolve(initializedComponents);
+    } catch (e) {
+      rejected(e);
+    }
+  });
+
+  return call;
 }
 
-function stop() {
+function stop(config) {
+  const call = new Promise((resolve, rejected) => {
+    const { cache } = config;
+    const componentHandler = cache.get('componentHandler');
+
+    if (componentHandler === undefined) {
+      throw new Error(`Whether you are calling .stop() before .start() or there
+      aren\'t any component to stop`);
+    }
+
+    try {
+      componentHandler.stopComponents();
+      resolve(true);
+    } catch (e) {
+      rejected(e);
+    }
+  });
+
+  return call;
 }
 
-function getInitializedComponents() {
+function getInitializedComponents(config) {
+  const call = new Promise((resolve, rejected) => {
+    const { cache } = config;
+    const initializedComponents = cache.get('initializedComponents');
+
+    if (initializedComponents === undefined) {
+      throw new Error(`Whether you are .getInitializedComponents() before .start() or there
+      aren\'t any initialized components`);
+    }
+
+    try {
+      resolve(initializedComponents);
+    } catch (e) {
+      rejected(e);
+    }
+  });
+
+  return call;
 }
 
 export default function dataapi(cfg) {
+  const cache = new Map();
   const config = defaults(cfg, {
     parentSelector: 'body',
     namespaces: ['api'],
+    cache
   });
-  const c1 = component(config);
-
-  c1.parseComponents();
 
   return {
-    start,
-    stop,
-    getInitializedComponents,
+    start: start.bind(null, config),
+    stop: stop.bind(null, config),
+    getInitializedComponents: getInitializedComponents.bind(null, config),
   };
 }
